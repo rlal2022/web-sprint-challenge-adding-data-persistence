c51f53bf27c420d1bbe05d843dae5f8612cb6e14
rlal2022 <lrahul2010@gmail.com>
2023-03-31 09:15:56 -0700

    maybe

--- api/task/model.js
+++ api/task/model.js
@@ -5,16 +5,12 @@
 const db = require("../../data/dbConfig.js");
 
 async function getTasks() {
-  const rows = await db("tasks as t")
-    .leftJoin("projects as p", "p.project_id", "t.project_id")
-    .select(
-      "t.task_id",
-      "t.task_description",
-      "t.task_notes",
-      "t.task_completed",
-      "p.project_name",
-      "p.project_description"
-    );
+  const rows = await db("tasks as t").leftJoin(
+    "projects as p",
+    "p.project_id",
+    "t.project_id"
+  );
+
   return rows;
 }
 
